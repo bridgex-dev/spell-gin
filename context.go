bb20aef435a643cf30340bef8bf0707f79509383
Andrei Bardziukou <arey@hey.com>
2022-09-13 19:19:57 +0200

    Overwrite WriteHeaderNow with spell headers

--- context.go
+++ context.go
@@ -5,7 +5,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type ResponseWriter struct {
+	gin.ResponseWriter
+	spell *spell.Context
+}
+
+func (w *ResponseWriter) WriteHeaderNow() {
+	_ = w.spell.WriteHeader()
+	w.ResponseWriter.WriteHeaderNow()
+}
+
+func NewResponseWriter(r *gin.Context, spell *spell.Context) *ResponseWriter {
+	return &ResponseWriter{r.Writer, spell}
+}
+
 type Context struct {
 	*gin.Context
 	Spell *spell.Context
+}
+
+func NewContext(r *gin.Context, spell *spell.Context) *Context {
+	// wrap WriteHeaderNow to write spell headers
+	r.Writer = NewResponseWriter(r, spell)
+
+	return &Context{
+		r,
+		spell,
+	}
 }
